0ee7a91fab0e0f7ce5b381f9297bb56f66299883
Lucas Sherwin <lksherwin@gmail.com>
2020-01-29 20:41:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(arr)
+  new_arr = []
+  i = 0
+  while i < arr.length do
+    new_arr.push(yield(arr[i]))
+    i += 1
+  end
+  new_arr
+end
+
+def reduce(arr, start = nil)
+  if start
+    total = start
+    i = 0
+  else
+    total = arr[0]
+    i = 1
+  end
+  while i < arr.length do
+    total = yield(total, arr[i])
+    i += 1
+  end
+  total
+end
