a3cf467fb9851b798b555cca8652bc467b7d3701
Lucas Sherwin <lksherwin@gmail.com>
2020-01-29 20:40:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,10 @@ def map(arr)
 end
 
 def reduce(arr, start = nil)
-  total = start
+  if start
+    total = start
+  else
+    total = arr[0]
   i = 0
   while i < arr.length do
     total = yield(total, arr[i])
